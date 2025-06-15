2bf8b615f40254c65cd73674fa7cc5689dd3ae49
Kannan Manivannan <verizonkannan@gmail.com>
2025-06-15 19:11:57 +0530

    Gradle Project structure Committed

---
+++ src/main/java/com/geflas/demo/FileUploadController.java
@@ -0,0 +1,12 @@
+package com.geflas.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FileUploadController {
+    @GetMapping("/name")
+    public String getName(){
+        return "Kannan";
+    }
+}
